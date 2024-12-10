20871095489f3fd0e774d9b04be654c871e8508c
ymoneych <ymoneych@gmail.com>
2024-12-10 14:43:26 +0900

    Change options

--- 02-preprocess_for_inference.sh
+++ 02-preprocess_for_inference.sh
@@ -1,28 +1,36 @@
+# RAW_DIR=/path/to/raw/dir
+# PROCESSED_DIR=/path/to/result/dir
+
+RAW_DIR=/home/shared/image/inhouse/lunit_ICI/images
+PROCESSED_DIR=input/lunit/lung
+
 
 # Preprocess ST data for inference (only WSI)
 python preprocess/CLAM/create_patches_fp.py \
-        --source $DATA_DIRECTORY \
-        --save_dir $RESULTS_DIRECTORY \
+        --source $RAW_DIR \
+        --save_dir $PROCESSED_DIR \
         --patch_size 256 \
         --seg \
         --patch \
         --stitch \
         --patch_level 1
 
-### Global features
-python preprocess/extract_img_features.py  \
-        --wsi_dataroot $INPUT_DIR'/patches' \
-        --patch_dataroot $DIR_TO_COORDS'/patches' \
-        --embed_dataroot $NEIGHBOR_FEATURES_DIRECTORY \
-        --slide_ext $EXTENSION \
-        --num_n 1
+EXTENSION='.mrxs'
 
-### Neighbor features
-python preprocess/extract_img_features.py  \
-        --wsi_dataroot $INPUT_DIR'/patches' \
-        --patch_dataroot $DIR_TO_COORDS'/patches' \
-        --embed_dataroot $NEIGHBOR_FEATURES_DIRECTORY \
-        --slide_ext $EXTENSION \
-        --use_openslide \
-        --num_n 5
+# ### Global features
+# python preprocess/extract_img_features.py  \
+#         --wsi_dataroot $RAW_DIR \
+#         --patch_dataroot $PROCESSED_DIR'/patches' \
+#         --embed_dataroot $PROCESSED_DIR'/emb/global' \
+#         --slide_ext $EXTENSION \
+#         --num_n 1
+
+# ### Neighbor features
+# python preprocess/extract_img_features.py  \
+#         --wsi_dataroot $RAW_DIR \
+#         --patch_dataroot $PROCESSED_DIR'/patches' \
+#         --embed_dataroot $PROCESSED_DIR'/emb/neighbor' \
+#         --slide_ext $EXTENSION \
+#         --use_openslide \
+#         --num_n 5
         
