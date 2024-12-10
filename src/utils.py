784997bacd9c8f41cac450ed8d9125c97d3d7220
ymoneych <ymoneych@gmail.com>
2024-12-10 21:33:46 +0900

    <feat>: Add function to get pos

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
+
+import numpy as np
+import pandas as pd
+
 from hest import STReader, VisiumReader, VisiumHDReader, XeniumReader
 
 
-# st_type = ['ST', 'Visium', 'Xenium']
 def load_st(path, platform):
     assert platform in ['st', 'visium', 'visium-hd', 'xenium'], "platform must be one of ['st', 'visium', 'visium-hd', 'xenium']"
     
@@ -19,3 +22,44 @@ def load_st(path, platform):
         
     return st
 
+def map_values(arr, step_size=256):
+    """
+    Map NumPy array values to integers such that:
+    1. The minimum value is mapped to 0
+    2. Values within 256 of each other are mapped to the same integer
+    
+    Args:
+    arr (np.ndarray): Input NumPy array of numeric values
+    
+    Returns:
+    tuple: 
+        - NumPy array of mapped integer values 
+    """
+    if arr.size == 0:
+        return np.array([]), {}
+    
+    # Sort the unique values
+    unique_values = np.sort(np.unique(arr))
+    
+    mapping = {}
+    current_key = 0
+    
+    mapping[unique_values[0]] = 0
+    current_value = unique_values[0]
+
+    for i in range(1, len(unique_values)):
+        if unique_values[i] - current_value > step_size:
+            current_key += 1
+            current_value = unique_values[i] 
+        
+        mapping[unique_values[i]] = current_key
+    
+    mapped_arr = np.vectorize(mapping.get)(arr)
+    
+    return mapped_arr
+
+def pxl_to_array(pixel_crds, step_size):
+    x_crds = map_values(pixel_crds[:,0], step_size)
+    y_crds = map_values(pixel_crds[:,1], step_size)
+    dst = np.stack((x_crds, y_crds), axis=1)
+    return dst
