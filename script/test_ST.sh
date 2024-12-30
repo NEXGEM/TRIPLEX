50ec9550a0c907bd55eaa8c89dbfcfc13e40e4c8
ymoneych <ymoneych@gmail.com>
2024-12-30 10:48:24 +0900

    chore: Test scripts for ST data

--- script/test_ST.sh
+++ script/test_ST.sh
@@ -1,9 +1,30 @@
-MODEL=TRIPLEX
 NUM_GPU=1
 MODE=eval
 
+MODEL=TRIPLEX
 datasets=( andersson andrew bryan )
 for dataset in  "${datasets[@]}"; do
-    echo Run training for 'ST/'$dataset'/TRIPLEX'
+    echo Run training for 'ST/'$dataset'/'$MODEL
     sbatch script/slurm.sh 'ST/'$dataset'/'$MODEL $NUM_GPU $MODE
+done
+
+MODEL=BLEEP
+datasets=( andersson andrew bryan )
+for dataset in  "${datasets[@]}"; do
+    echo Run training for 'ST/'$dataset'/'$MODEL
+    sbatch --gres=gpu:$NUM_GPU script/slurm.sh 'ST/'$dataset'/'$MODEL $NUM_GPU $MODE
+done
+
+MODEL=StNet
+datasets=( andersson andrew bryan )
+for dataset in  "${datasets[@]}"; do
+    echo Run training for 'ST/'$dataset'/'$MODEL
+    sbatch --gres=gpu:$NUM_GPU script/slurm.sh 'ST/'$dataset'/'$MODEL $NUM_GPU $MODE
+done
+
+MODEL=EGN
+datasets=( andersson andrew bryan )
+for dataset in  "${datasets[@]}"; do
+    echo Run training for 'ST/'$dataset'/'$MODEL
+    sbatch --gres=gpu:$NUM_GPU script/slurm.sh 'ST/'$dataset'/'$MODEL $NUM_GPU $MODE
 done
--- script/train_ST.sh
+++ script/train_ST.sh
@@ -1,12 +1,12 @@
 NUM_GPU=1
 MODE=cv
 
-# MODEL=TRIPLEX
-# datasets=( andersson andrew bryan )
-# for dataset in  "${datasets[@]}"; do
-#     echo Run training for 'ST/'$dataset'/'$MODEL
-#     sbatch script/slurm.sh 'ST/'$dataset'/'$MODEL $NUM_GPU $MODE
-# done
+MODEL=TRIPLEX
+datasets=( andersson andrew bryan )
+for dataset in  "${datasets[@]}"; do
+    echo Run training for 'ST/'$dataset'/'$MODEL
+    sbatch script/slurm.sh 'ST/'$dataset'/'$MODEL $NUM_GPU $MODE
+done
 
 MODEL=BLEEP
 datasets=( andersson andrew bryan )
@@ -23,7 +23,9 @@ for dataset in  "${datasets[@]}"; do
 done
 
 MODEL=EGN
-datasets=( andersson andrew bryan )
+datasets=( andersson andrew )
+# datasets=( andersson andrew bryan )
+# datasets=( bryan )
 for dataset in  "${datasets[@]}"; do
     echo Run training for 'ST/'$dataset'/'$MODEL
     sbatch --gres=gpu:$NUM_GPU script/slurm.sh 'ST/'$dataset'/'$MODEL $NUM_GPU $MODE
