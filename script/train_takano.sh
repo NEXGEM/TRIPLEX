648cd75ce97b0528055e239fc0455cc87ef74675
ymoneych <ymoneych@gmail.com>
2025-02-20 22:27:58 +0900

    chore: Add config and script for each model
    - Add configs for BLEEP, EGN, StNet
    - Fix num_fold (6 -> 5)

--- script/train_takano.sh
+++ script/train_takano.sh
@@ -1,5 +1,16 @@
 NUM_GPU=4
 MODE=cv
 
-MODEL=TRIPLEX
+# MODEL=TRIPLEX
+# sbatch --gres=gpu:$NUM_GPU --ntasks-per-node=$NUM_GPU --mem-per-gpu=64G --cpus-per-gpu=6 script/slurm.sh "takano/xenium/$MODEL" $NUM_GPU $MODE
+
+# MODEL=StNet
+# sbatch --gres=gpu:$NUM_GPU --ntasks-per-node=$NUM_GPU --mem-per-gpu=64G --cpus-per-gpu=6 script/slurm.sh "takano/xenium/$MODEL" $NUM_GPU $MODE
+
+# MODEL=EGN
+# sbatch --gres=gpu:$NUM_GPU --ntasks-per-node=$NUM_GPU --mem-per-gpu=64G --cpus-per-gpu=6 script/slurm.sh "takano/xenium/$MODEL" $NUM_GPU $MODE
+
+MODEL=BLEEP
 sbatch --gres=gpu:$NUM_GPU --ntasks-per-node=$NUM_GPU --mem-per-gpu=64G --cpus-per-gpu=6 script/slurm.sh "takano/xenium/$MODEL" $NUM_GPU $MODE
+
+
