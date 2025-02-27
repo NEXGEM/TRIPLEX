a8f8a5ab25752ac495e9ebaae876c8cfe1699b6c
ymoneych <ymoneych@gmail.com>
2025-02-27 14:39:03 +0900

    fix: Fix agg_results to address more cases

--- src/experiment/agg_results.py
+++ src/experiment/agg_results.py
@@ -10,8 +10,20 @@ def main(args):
     
     results = []
     for dataset in glob(f"{args.log_dir}/{args.dataset}/*"):
-        dataset_name = dataset.split('/')[-1]
-        path = glob(f"{dataset}/{args.model}/*")[-1]
+        
+        if not os.path.isdir(f"{dataset}/{args.model}"):
+            if args.model in dataset:
+                dataset_name = args.dataset
+                # path = glob(f"{args.log_dir}/{args.dataset}/{args.model}/*")[-1]
+                path = f"{args.log_dir}/{args.dataset}/{args.model}/{args.log_name}"
+            else:
+                continue
+        
+        else:
+            dataset_name = dataset.split('/')[-1]
+            # path = glob(f"{dataset}/{args.model}/*")[-1]
+            path = f"{dataset}/{args.model}/{args.log_name}"
+            
         
         for fold_path in glob(f"{path}/*"):
             if not os.path.exists(f"{fold_path}/eval/metrics.csv"):
@@ -32,13 +44,13 @@ def main(args):
             
             for col in test_cols:
                 metric_lists[col].append(best_metrics[col])
-            
+                    
         results.append({
             'Dataset': dataset_name,
             **{f"{col.replace('test_', '')}_mean": np.mean(metric_lists[col]) for col in test_cols},
             **{f"{col.replace('test_', '')}_std": np.std(metric_lists[col]) for col in test_cols}
         })
-
+        
     results_df = pd.DataFrame(results)
     means = results_df.iloc[:, 1:].mean()
     results_df.loc[len(results_df)] = ['Average'] + list(means)
@@ -52,7 +64,8 @@ if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--model', type=str, default='TRIPLEX')
     argparser.add_argument('--log_dir', type=str, default='./logs')
-    argparser.add_argument('--dataset', type=str, default='hest')
+    argparser.add_argument('--log_name', type=str, default='2025-02-24-14-35')
+    argparser.add_argument('--dataset', type=str, default='GSE240429')
     argparser.add_argument('--output_dir', type=str, default='./output/res')
     args = argparser.parse_args()
     
