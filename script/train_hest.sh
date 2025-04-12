d57ca05a6cbd8f2a62dd075c8aaf66497bf519b6
ymoneych <ymoneych@gmail.com>
2025-04-12 17:35:28 +0900

    Update the script for training hest

--- script/train_hest.sh
+++ script/train_hest.sh
@@ -1,18 +1,38 @@
 
-ulimit -n 100000
-export NCCL_P2P_LEVEL=NVL
-MODEL=TRIPLEX
+#!/bin/bash
+NUM_GPU=1
+MODE=cv
 
-datasets=$(ls config/hest)
-for dataset in $datasets; do
-    CUDA_VISIBLE_DEVICES=6,7 python src/main.py --config_name hest/$dataset/$MODEL --gpu 2 --mode cv
-done
+run_training() {
+    local model=$1
+    shift
+    local datasets=("$@")
+    local sbatch_opts=""
 
-# datasets=( HCC IDC LUNG LYMPH_IDC PAAD PRAD READ SKCM )
-# for dataset in "${datasets[@]}"; do
-#     CUDA_VISIBLE_DEVICES=6,7 python src/main.py \
-#                 --config_name hest/$dataset/TRIPLEX  \
-#                 --mode cv \
-#                 --gpu 2 \
-#                 --mode cv
-# done
+    # Use GPU option for all models except TRIPLEX
+    # if [[ "$model" != "TRIPLEX" ]]; then
+    #     sbatch_opts="--gres=gpu:$NUM_GPU"
+    # fi
+    sbatch_opts="--gres=gpu:$NUM_GPU --ntasks-per-node=$NUM_GPU --mem-per-gpu=62G --cpus-per-gpu=6"
+
+    for dataset in "${datasets[@]}"; do
+        echo "Run training for ${dataset}/${model}"
+        sbatch ${sbatch_opts} script/slurm.sh "${dataset}/${model}" $NUM_GPU $MODE
+    done
+}
+
+datasets=( 
+    hest/bench_data/CCRCC 
+    hest/bench_data/COAD 
+    hest/bench_data/HCC 
+    hest/bench_data/IDC 
+    hest/bench_data/LUNG 
+    hest/bench_data/LYMPH_IDC 
+    hest/bench_data/PAAD 
+    hest/bench_data/PRAD 
+    hest/bench_data/READ 
+    hest/bench_data/SKCM 
+)
+
+# datasets=( bench_data/COAD bench_data/LYMPH_IDC )
+run_training "TRIPLEX" "${datasets[@]}"
